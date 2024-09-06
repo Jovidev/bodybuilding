dae35f0539c48b3e173a47611699507c1b8b8c06
Jovidev <Vitorskyline17@gmail.com>
2024-09-06 16:41:33 -0300

    pagina inicial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-
 function App() {
   return (
-    <div className="App">
-      <h1>teste </h1>
+    <div className="container">
+      <h1>Bem vindo</h1>
+
+      <div className="containerInput">
+        <h3>Faça o login</h3>
+        <input id="login" type="text" placeholder="Login" />
+        <input id="senha" type="text" placeholder="Senha" />
+      </div>
+
+      <div className="containerButton">
+        <button>Login</button>
+        <a href="senha.js">Esqueci minha senha</a>
+        <button>Cadastre-se</button>
+      </div>
+      
     </div>
   );
 }
